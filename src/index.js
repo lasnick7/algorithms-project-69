11bf9a62de25913ab13e857e899705a8f5dfe50e
Nikita Laskovykh <lasnick711@gmail.com>
2025-03-12 18:41:07 +0300

    indents 2

--- src/index.js
+++ src/index.js
@@ -1,98 +1,105 @@
 /* eslint-disable  linebreak-style */
 
 function makeTerm(token) {
-    const matchResult = token.match(/\w+/g); 
-    return matchResult ? matchResult[0] : ''; 
+  const matchResult = token.match(/\w+/g);
+  return matchResult ? matchResult[0] : '';
 }
 
 function makeArrayFromText(text) {
-    return text.toLowerCase().split(' ').map((token) => makeTerm(token));
+  return text
+    .toLowerCase()
+    .split(' ')
+    .map((token) => makeTerm(token));
 }
 
 function countEntries(doc, word) {
-    let count = 0;
+  let count = 0;
 
-    const arrayOfWords = makeArrayFromText(doc.text);
+  const arrayOfWords = makeArrayFromText(doc.text);
 
-    for (let item of arrayOfWords) {
-        if (item === word) {
-            count += 1;
-        }
+  for (let item of arrayOfWords) {
+    if (item === word) {
+      count += 1;
     }
-    console.log(`entries for ${doc.id}, word ${word}: ${count}`)
-    return count;
+  }
+  console.log(`entries for ${doc.id}, word ${word}: ${count}`);
+  return count;
 }
 
 function invertIndex(docs) {
-    const index = {};
+  const index = {};
 
-    const docsWithArrText = docs.map((doc) => {
-        const newDoc = {...doc};
+  const docsWithArrText = docs.map((doc) => {
+    const newDoc = { ...doc };
 
-        const arrayOfWords = makeArrayFromText(doc.text);
+    const arrayOfWords = makeArrayFromText(doc.text);
 
-        newDoc.text = arrayOfWords;
-        return newDoc;
-    });
+    newDoc.text = arrayOfWords;
+    return newDoc;
+  });
 
-    docsWithArrText.forEach((doc) => doc.text.forEach((word) => {
-        if(!Object.keys(index).includes(word)) {
-            index[word] = [];
-        }
-        if (!index[word].includes(doc.id)) {
-            index[word].push(doc.id)
-        }
-    }));
+  docsWithArrText.forEach((doc) =>
+    doc.text.forEach((word) => {
+      if (!Object.keys(index).includes(word)) {
+        index[word] = [];
+      }
+      if (!index[word].includes(doc.id)) {
+        index[word].push(doc.id);
+      }
+    })
+  );
 
-    return index;
+  return index;
 }
 
 function TF(doc, word) {
-    const arrayOfWords = makeArrayFromText(doc.text);
-    
-    const wordCount = arrayOfWords.length;
+  const arrayOfWords = makeArrayFromText(doc.text);
 
-    const entriesCount = countEntries(doc, word);
+  const wordCount = arrayOfWords.length;
 
-    return entriesCount / wordCount;
+  const entriesCount = countEntries(doc, word);
+
+  return entriesCount / wordCount;
 }
 
 function IDF(docs, word, index) {
-    const docsCount = docs.length;
-    if (index[word]) {
-        const termCount = index[word].length || 0;
-        return Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5));
-    }
-    return 0;
+  const docsCount = docs.length;
+  if (index[word]) {
+    const termCount = index[word].length || 0;
+    return Math.log2(1 + (docsCount - termCount + 1) / (termCount + 0.5));
+  }
+  return 0;
 }
 
 export default function search(docs, items) {
-    
-    const index = invertIndex(docs);
+  const index = invertIndex(docs);
 
-    const wordsToSearch = makeArrayFromText(items);
+  const wordsToSearch = makeArrayFromText(items);
 
-    const result = docs.map((doc) => {
-        let TFIDF = 0;
+  const result = docs.map((doc) => {
+    let TFIDF = 0;
 
-        wordsToSearch.forEach((word) => {
-            const currWordTF = TF(doc, word);
-            const currWordIDF = IDF(docs, word, index);
-            const currWordTFIDF = currWordTF * currWordIDF;
+    wordsToSearch.forEach((word) => {
+      const currWordTF = TF(doc, word);
+      const currWordIDF = IDF(docs, word, index);
+      const currWordTFIDF = currWordTF * currWordIDF;
 
-            console.log(`word ${word} in doc ${doc.id} TF: ${currWordTF}, IDF: ${currWordIDF}, TF-IDF: ${currWordTFIDF}`)
-            TFIDF += currWordTFIDF;
-        });
+      console.log(
+        `word ${word} in doc ${doc.id} TF: ${currWordTF}, IDF: ${currWordIDF}, TF-IDF: ${currWordTFIDF}`
+      );
+      TFIDF += currWordTFIDF;
+    });
 
-        return {
-            id: doc.id, 
-            TFIDF,
-        };
-    }); 
+    return {
+      id: doc.id,
+      TFIDF,
+    };
+  });
 
-    console.log(result); 
+  console.log(result);
 
-    return result.filter((doc) => doc.TFIDF > 0)
-        .sort((a, b) => b.TFIDF - a.TFIDF)
-        .map((doc) => doc.id);
+  return result
+    .filter((doc) => doc.TFIDF > 0)
+    .sort((a, b) => b.TFIDF - a.TFIDF)
+    .map((doc) => doc.id);
 }
