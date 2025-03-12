89d130f1ffba0b9f2286a585a10275db5d69f30f
Nikita Laskovykh <lasnick711@gmail.com>
2025-03-12 12:54:51 +0300

    step 1

---
+++ Makefile
@@ -0,0 +1,11 @@
+t:
+	node functions.js
+
+s:
+	node src/search.js
+
+i:
+	node src/index.js
+
+test:
+	NODE_OPTIONS=--experimental-vm-modules npx jest
---
+++ __tests__/index.test.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+
+import search from "../src/index.js";
+
+const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
+const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+const doc3 = { id: 'doc3', text: "I'm your shooter." };
+const docs = [doc1, doc2, doc3];
+
+test('search', () => {
+  expect(search(docs, 'shoot')).toEqual(['doc1', 'doc2']);
+  expect(search([], 'shoot')).toEqual([]);
+});
